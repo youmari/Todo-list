4748263e188e78cc3da8b8ca6480baa6fbf613fd
youmari <youmari300@gmail.com>
2021-11-30 14:07:12 +0100

    Create function to generate list items dynamically

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import listData from "./js/data.js";
+import './css/style.css';
 
-function listObject(description,completed,number) {
-  this.description = description;
-  this.completed = completed;
-  this.number = number;
-}
 
-const createElements = () => {
+const displayItems = () => {
+  const ulContainer = document.querySelector('.todo-container');
+  const addInput = document.createElement('input');
+  const clearBtn = document.createElement('button');
+
+  ulContainer.textContent = `Today's To Do`;
+  ulContainer.append(addInput);
+  addInput.classList.add('add-input');
+  addInput.type = 'text';
+  addInput.placeholder = 'Add to your list...'
+
+  listData.forEach((item) => {
+    const listElement = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const labelItem = document.createElement('label');
+
+    ulContainer.append(listElement);
+    listElement.append(checkbox,labelItem)
+
+    checkbox.type = 'checkbox';
+    checkbox.id = item.index;
+    checkbox.name = item.index;
+    checkbox.classList.add('checkbox')
+    labelItem.setAttribute('for', item.index);
+    labelItem.textContent = item.description;
+  });
+
+  ulContainer.append(clearBtn);
+  clearBtn.classList.add('clear-btn');
+  clearBtn.textContent = 'Clear all completed';
   
 
 }
+displayItems();
+
