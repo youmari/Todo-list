495bd4d4638d6f6bcecf07fc6babe37481e48b6a
youmari <youmari300@gmail.com>
2021-11-30 14:05:56 +0100

    Add data in array of objects

--- src/js/data.js
+++ src/js/data.js
@@ -1,3 +1,25 @@
-const listData = [];
+const listData = [
+  {
+    description: 'go to the gym',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'finish my tasks for today',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'read my book',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'going for a walk',
+    completed: false,
+    index: 3,
+  },
 
-export default listData
+];
+
+export default listData;
