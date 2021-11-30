83c9cb5791524bf11aa47388bfd45213700142fb
youmari <youmari300@gmail.com>
2021-11-30 11:51:48 +0100

    Add function to generate list objects

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import listData from "./js/data.js";
+
+function listObject(description,completed,number) {
+  this.description = description;
+  this.completed = completed;
+  this.number = number;
+}
+
+const createElements = () => {
+  
+
+}
