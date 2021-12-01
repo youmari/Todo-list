cd5d5a032b864d99f48dbfe66ebf14a6d1e335e7
youmari <youmari300@gmail.com>
2021-12-01 13:34:45 +0100

    Add updateStatus function to update the status of items in the list

---
+++ src/js/updateItems.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import listData from '../index';
+import { storeListItemData } from './localStorageFunctions';
+
+const updateItemsStatus = (checkbox, item) => {
+  checkbox.addEventListener('change', () => {
+    !item.completed
+      ? item.completed = true
+      : item.completed = false;
+      storeListItemData(listData);
+  });
+};
+
+export default updateItemsStatus;
