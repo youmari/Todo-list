e7fdd36ea51a87a164f6fe004ac1d712969e604c
youmari <youmari300@gmail.com>
2021-12-01 13:33:00 +0100

    Change index.js so that we can update item status

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-import listData from './js/data';
+/* eslint-disable */
 import './css/style.css';
 import enter from './assets/enter.png';
 import dots from './assets/three.png';
 import recycle from './assets/recycle.png';
+import updateItemsStatus from './js/updateItems';
+import { getListItemdata } from './js/localStorageFunctions';
 
-const displayItems = () => {
-  const ulContainer = document.querySelector('.todo-container');
-  const addInput = document.createElement('input');
-  const clearBtn = document.createElement('button');
+let listData = [
+  {
+    description: 'go to the gym',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'finish my tasks for today',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'read my book',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'going for a walk',
+    completed: false,
+    index: 3,
+  },
+
+];
+const ulContainer = document.querySelector('.todo-container');
+const addInput = document.createElement('input');
+const clearAllCompletedBtn = document.createElement('button');
+const clearIcon = document.createElement('img');
+const inputBtn = document.createElement('img');
+
+const createTodoListElement = () => {
   const title = document.createElement('h2');
-  const clearIcon = document.createElement('img');
-  const inputBtn = document.createElement('img');
   const titleAndIconDiv = document.createElement('div');
   const inputAndEnterDiv = document.createElement('div');
 
@@ -25,31 +51,43 @@ const displayItems = () => {
   addInput.classList.add('add-input');
   addInput.type = 'text';
   addInput.placeholder = 'Add to your list...';
+};
 
+const createItemsElements = (item) => {
+  const threeDotIcon = document.createElement('img');
+  const listElement = document.createElement('li');
+  const checkbox = document.createElement('input');
+  const labelItem = document.createElement('label');
+  const itemDiv = document.createElement('div');
+
+  ulContainer.append(listElement);
+  listElement.append(itemDiv, threeDotIcon);
+  itemDiv.append(checkbox, labelItem);
+  itemDiv.classList.add('item-div');
+  threeDotIcon.src = dots;
+  threeDotIcon.classList.add('dots');
+  checkbox.type = 'checkbox';
+  if (item.completed === true) checkbox.checked = true;
+  checkbox.id = item.index;
+  checkbox.name = item.index;
+  checkbox.classList.add('checkbox');
+  labelItem.setAttribute('for', item.index);
+  labelItem.textContent = item.description;
+  updateItemsStatus(checkbox, item);
+};
+
+const displayItems = () => {
+  const newData = getListItemdata();
+  if (newData !== null) listData = newData;
+  createTodoListElement();
   listData.forEach((item) => {
-    const threeDotIcon = document.createElement('img');
-    const listElement = document.createElement('li');
-    const checkbox = document.createElement('input');
-    const labelItem = document.createElement('label');
-    const itemDiv = document.createElement('div');
-
-    ulContainer.append(listElement);
-    listElement.append(itemDiv, threeDotIcon);
-    itemDiv.append(checkbox, labelItem);
-    itemDiv.classList.add('item-div');
-    threeDotIcon.src = dots;
-    threeDotIcon.classList.add('dots');
-    checkbox.type = 'checkbox';
-    checkbox.id = item.index;
-    checkbox.name = item.index;
-    checkbox.classList.add('checkbox');
-    labelItem.setAttribute('for', item.index);
-    labelItem.textContent = item.description;
+    createItemsElements(item);
   });
-
-  ulContainer.append(clearBtn);
-  clearBtn.classList.add('clear-btn');
-  clearBtn.textContent = 'Clear all completed';
+  ulContainer.append(clearAllCompletedBtn);
+  clearAllCompletedBtn.classList.add('clear-btn');
+  clearAllCompletedBtn.textContent = 'Clear all completed';
 };
 
 displayItems();
+
+export default listData;
